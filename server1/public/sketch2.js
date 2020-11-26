3a4874706aa85b11b93d7a2be4a485aab1290f50
gabriellahartanto <gabriella.hartanto@icloud.com>
2020-11-27 05:31:16 +0700

    fix overall

--- server1/public/sketch2.js
+++ server1/public/sketch2.js
@@ -5,11 +5,16 @@ var showNote = false;
 var color = 'orange';
 
 function setup() {
+  // create new canvas with size of 450x450
   let canvas = createCanvas(450, 450);
+  // save canvas in a <div> in index.html
   canvas.parent('canvas');
+
+  // put conditions on the canvas to start or stop drawing
   canvas.mousePressed(startPath);
   canvas.mouseReleased(endPath);
 
+  // set buttons to select the post-it color
   let orange = select('#btn-orange');
   orange.mousePressed(() => {
     showNote = true;
@@ -40,31 +45,46 @@ function setup() {
     color = 'pink';
   });
 
+  // set an undo button to remove the last wrong stroke
+  let undo = select('#btn-undo');
+  undo.mousePressed(() => {
+    drawing.pop();
+  })
+
+  // set a done button to save the sketch when done
   let done = select('#btn-done');
   done.mousePressed(saveDrawing);
 }
 
+// to start a stroke
 function startPath() {
   isDrawing = true;
+  // always make an empty array when starting a new stroke
   currentPath = [];
+  // save the stroke with the rest of the drawing
   drawing.push(currentPath);
 }
 
+// to end a stroke
 function endPath() {
   isDrawing = false;
 }
 
 function draw() {
+  // set background to white
   background(255);
 
+  // check if currently drawing a stroke
   if (isDrawing) {
     const point = {
       x: mouseX,
       y: mouseY
     }
+    // saves current point to the path
     currentPath.push(point);
   }
 
+  // if the user chooses the post-it color, change fill color accordingly
   if (showNote) {
     noStroke();
     if (color === 'orange') {
@@ -78,25 +98,33 @@ function draw() {
     } else if (color === 'pink') {
       fill('#FF7BAC');
     }
+    // creates a square as the post it
     square(0, 0, 450);
   }
 
+  // to draw everything
   stroke(0);
   strokeWeight(4);
   noFill();
+  // loop through the entire drawing
   for (let i = 0; i < drawing.length; i++) {
+    // start connecting the points saved
     beginShape();
     for (let j = 0; j < drawing[i].length; j++) {
       vertex(drawing[i][j].x, drawing[i][j].y);
     }
+    // end the connection
     endShape();
   }
 }
 
+// save the drawing to Firebase Realtime Database
 function saveDrawing() {
+  // check if the user have drawn/written anything on the post it
   if (drawing.length > 0) {
     const ref = database.ref('drawings');
-    // const rand = Math.round(Math.random() * 2000);
+    // saves the color of the post it, all the points in the drawing, 
+    // and the random location on the big moodboard later on
     const data = {
       color,
       drawing,
@@ -105,11 +133,13 @@ function saveDrawing() {
         y: Math.round(Math.random() * 2000)
       }
     }
+    // saves the new data to Firebase
     ref.push(data, (err, res) => {
       if (err) {
         console.log('ERROR!');
       } else {
         console.log('SUCCESS');
+        window.location.href="thanks.html";
       }
     });
   }
--- server2/public/sketch.js
+++ server2/public/sketch.js
@@ -1,33 +1,69 @@
 var big = [];
 var note = '';
+var hm = 2;
+var stop = false;
+var a = 0.0;
+var s = 0.0;
 
 function setup() {
+  // creates new canvas for the big moodboard 2000x800
   let canvas = createCanvas(2000, 800);
-
+  // save canvas in a <div> in index.html
   canvas.parent('canvas');
 
+  // connects to Firebase to show the drawings
   var ref = database.ref('drawings');
   ref.on('value', (data) => {
     const drawings = data.val();
+    // get each key (sketch/note) from drawings
     const keys = Object.keys(drawings);
+    // for each sketch/note, show the drawing on the moodboard
     for (let i = 0; i < keys.length; i++) {
       showDrawing(keys[i]);
     }
   }, (err) => {
     console.log(err);
   });
+
+  // if (!stop) {
+    setInterval(() => {
+      hm -= 0.6;
+    }, 2000);
+  // }
 }
 
 function draw() {
+  // set background color to white
   background(255);
 
+  // make the drawings smaller to fit in everything
   scale(0.3);
   
+  // go through each user
   for (let k = 0; k < big.length; k++) {
     if (k === big.length - 1) {
       // DO SMTHG
+
+      // a = a + 0.04;
+      // s = cos(a) * 2;
+    
+      // //Translate our rectangle from the origin to the middle of
+      // //the canvas, then scale it with 's'
+      // translate(width / 2, height / 2);
+      // scale(s);
+
+      let num = 1 * hm;
+      console.log(num);
+      if (num > 0.3) {
+        // console.log('HA!');
+        scale(num);
+        stop = true;
+      }
+      
     }
+
     noStroke();
+    // changes the color of the post-it accordingly
     if (big[k].color === 'orange') {
       fill('#FF931E');
     } else if (big[k].color === 'green') {
@@ -39,24 +75,32 @@ function draw() {
     } else if (big[k].color === 'pink') {
       fill('#FF7BAC');
     }
-    square(big[k].location.x, big[k].location.y, 450);
+    // makes a square as a post-it
+    square(big[k].location.x + 500, big[k].location.y + 500, 450);
+    // loop through each of the strokes in the drawing
     for (let i = 0; i < big[k].drawing.length; i++) {
+      // start connecting the points
       beginShape();
       stroke(0);
       strokeWeight(4);
       noFill();
       for (let j = 0; j < big[k].drawing[i].length; j++) {
-        vertex(big[k].drawing[i][j].x + big[k].location.x, big[k].drawing[i][j].y + big[k].location.y);
+        // sets the drawing in a random position
+        vertex(big[k].drawing[i][j].x + big[k].location.x + 500, big[k].drawing[i][j].y + big[k].location.y + 500);
       }
+      // end connection
       endShape();
     }
   }
 }
 
+// takes data from the realtime databse
 function showDrawing(key) {
   let ref = database.ref(`drawings/${key}`);
+  // takes all the data for each key
   ref.on('value', (data) => {
     let image = data.val();
+    // saves it to the 'big' board
     big.push(image);
   }, (err) => {
     console.log(err);
